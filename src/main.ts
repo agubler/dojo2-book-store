264bd95c1ef88eae7d4b9329bd26e05b84bb4495
Anthony Gubler <agubler@sitepen.com>
2016-08-04 12:11:27 +0100

    complete setup with working build

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
-export {
-	/* provide the public API here */
-};
+import createApp from 'dojo-app/createApp';
+import createWidget from 'dojo-widgets/createWidget';
+import createMemoryStore from 'dojo-widgets/util/createMemoryStore';
+
+const widgetStore = createMemoryStore({
+	data: [
+		{
+			id: 'book-store',
+			label: 'Book Store'
+		},
+		{
+			id: 'book-store-title',
+			label: 'Book Store Title'
+		}
+
+	]
+});
+
+const app = createApp({ defaultStore: widgetStore });
+
+app.loadDefinition({
+	widgets: [
+		{
+			id: 'book-store',
+			factory: createWidget
+		}
+	],
+	customElements: [
+		{
+			name: 'dojo-widget',
+			factory: createWidget
+		}
+	]
+
+});
+
+app.realize(document.body);
